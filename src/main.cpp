9259effaa2d77c7da5b4e57e4701ca11376bc271
Dimitrios Stoupis <dimitris.apple@gmail.com>
2019-07-31 12:48:01 +0000

    Possible pipeline fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <CCSDSTransferFrame.hpp>
 
 int main() {
-	CCSDSTransferFrame transferFrame = CCSDSTransferFrame(0, 3, 4);
+	//CCSDSTransferFrame transferFrame = CCSDSTransferFrame(0, 3, 4);
 
 	return 0;
 }
