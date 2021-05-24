b4a45971f33971b6c05d85e136c59557c9f381ae
Sedictious <ele.hatzy@gmail.com>
2021-05-24 18:53:51 +0300

    Refactor CLCW

--- inc/Packet.hpp
+++ inc/Packet.hpp
@@ -9,8 +9,6 @@
 
 class Packet;
 
-typedef etl::unique_ptr<Packet> UniquePacket;
-
 enum ServiceType {
     TYPE_A = 0,
     TYPE_B = 1
@@ -210,70 +208,70 @@ public:
      * @brief Field status is mission-specific and not used in the CCSDS data link protocol
      */
     const uint8_t field_status() const {
-        return (clcw_data[0] & 0x1C) >> 2U;
+        return fieldStatus;
     }
 
     /**
      * @brief COP version in effect
      */
     const uint8_t cop_in_effect() const {
-        return clcw_data[0] & 0x03;
+        return copInEffect;
     }
 
     /**
      * @brief Virtual Channel identifier of target virtual channel
      */
     const uint8_t vcid() const {
-        return (clcw_data[1] & 0xFC) >> 2U;
+        return vcId;
     }
 
     /**
      * @brief Indicates whether RF transmission in the physical channel is ready
      */
     const bool no_rf_avail() const {
-        return (clcw_data[2] & 0x80) >> 7U;
+        return noRFAvail;
     }
 
     /**
      * @brief Indicates whether "bit lock" is achieved
      */
     const bool no_bit_lock() const {
-        return (clcw_data[2] & 0x40) >> 6U;
+        return noBitLock;
     }
 
     /**
      * @brief Indicates whether FARM is in the Lockout state
      */
     const bool lockout() const {
-        return (clcw_data[2] & 0x20) >> 5U;
+        return lckout;
     }
 
     /**
      * @brief Indicates whether the receiver doesn't accept any packets
      */
     const bool wait() const {
-        return (clcw_data[2] & 0x10) >> 4U;
+        return wt;
     }
 
     /**
      * @brief Indicates whether there are Type-A Frames that will need to be retransmitted
      */
     const bool retransmission() const {
-        return (clcw_data[2] & 0x08) >> 3U;
+        return retransmit;
     }
 
     /**
      * @brief The two least significant bits of the virtual channel
      */
     const uint8_t farm_b_counter() const {
-        return (clcw_data[2] & 0x06) >> 1U;
+        return farmBCounter;
     }
 
     /**
      * @brief Next expected transfer frame number
      */
     const uint8_t report_value() const {
-        return clcw_data[3];
+        return reportValue;
     }
 
     // @todo Is this a bit risky, ugly and hacky? Yes, like everything in life. However, I still prefer this over
@@ -287,26 +285,58 @@ public:
         clcw_data[1] = data[1];
         clcw_data[2] = data[2];
         clcw_data[3] = data[3];
+
+        fieldStatus = (clcw_data[0] & 0x1C) >> 2U;
+        copInEffect = clcw_data[0] & 0x03;
+        vcId = (clcw_data[1] & 0xFC) >> 2U;
+        noRFAvail = (clcw_data[2] & 0x80) >> 7U;
+        noBitLock = (clcw_data[2] & 0x40) >> 6U;
+        lckout = (clcw_data[2] & 0x20) >> 5U;
+        wt = (clcw_data[2] & 0x10) >> 4U;
+        retransmit = (clcw_data[2] & 0x08) >> 3U;
+        farmBCounter = (clcw_data[2] & 0x06) >> 1U;
+        reportValue = clcw_data[3];
     }
 
     /**
      * @brief Control Link Words used for FARM reports
      */
     CLCW(const uint8_t field_status, const uint8_t cop_in_effect, const uint8_t virtual_channel, const bool no_rf_avail,
-         const bool no_bit_lock, const bool lockout, const bool wait, const bool retransmit,
+         const bool no_bit_lock, const bool lockout, const bool wait, const bool retransmission,
          const uint8_t farm_b_counter,
          const uint8_t report_value) {
         clcw_data[0] = (field_status << 2U) | cop_in_effect;
         clcw_data[1] = (virtual_channel << 2U);
         clcw_data[2] = (no_rf_avail << 7U) | (no_bit_lock << 6U) | (lockout << 5U) | (wait << 4U) |
-                       (retransmit << 3U) | (farm_b_counter << 1U);
+                       (retransmission << 3U) | (farm_b_counter << 1U);
         clcw_data[3] = report_value;
+
+        fieldStatus = field_status;
+        noRFAvail = no_rf_avail;
+        vcId = virtual_channel;
+        copInEffect = cop_in_effect;
+        lckout = lockout;
+        noBitLock = no_bit_lock;
+        wt = wait;
+        retransmit = retransmission;
+        farmBCounter = farm_b_counter;
+        reportValue = report_value;
     }
 
 private:
     uint8_t clcw_data[4];
     // This is used by COP to signal the higher procedures
     FDURequestType confSignal;
+    uint8_t fieldStatus;
+    uint8_t copInEffect;
+    uint8_t vcId;
+    bool noRFAvail;
+    bool noBitLock;
+    bool lckout;
+    bool retransmit;
+    bool wt;
+    uint8_t farmBCounter;
+    uint8_t reportValue;
 };
 
 #endif //CCSDS_PACKET_H
