9226a249b93bea6c5bd0f32178e890cb93bf001b
konkapo <kostaskapogl@gmail.com>
2021-10-09 21:42:41 +0300

    added Logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+#include "Logger.hpp"
+#include "CCSDSChannel.hpp"
+
 int main() {
-    return 0;
+	LOG_DEBUG << "CCSDS Services test application";
+
+	// Set up Service Channel
+	PhysicalChannel phy_channel_fop = PhysicalChannel(1024, false, 12, 1024, 220000, 20);
+
+	LOG_DEBUG << "CCSDS Services test application";
+
 }
