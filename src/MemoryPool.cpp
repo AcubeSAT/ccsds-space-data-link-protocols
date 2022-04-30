974d78a4898415ed529ced23eadced41b2f0d89e
Christos Siantsis <christossiantsis@gmail.com>
2022-04-30 18:03:15 +0300

    Resolved threads

--- src/MemoryPool.cpp
+++ src/MemoryPool.cpp
@@ -1,25 +1,20 @@
 
 #include "iostream"
-#include "MemoryPool.h"
+#include "MemoryPool.hpp"
 #include "cstring"
 #include "Logger.hpp"
+#include "Alert.hpp"
 
 
 MemoryPool::MemoryPool() {
 
 }
 
-/**
- * Method that copies the packet data to the first available chunk of memory of the memory pool.
- * Calls the findFit method in order to find the index of the array that is first available.
- * @param packet pointer to the packet data.
- * @param packetLength the length of the packet data.
- * @return an uint8_t pointer to the packet data in the memory pool.
- */
 
 uint8_t *MemoryPool::allocatePacket(uint8_t *packet, uint16_t packetLength) {
-    int start = findFit(packetLength);
-    if (start == -1) {
+    std::pair<uint16_t, MasterChannelAlert> index = findFit(packetLength);
+    uint16_t start = index.first;
+    if (index.second == NO_SPACE) {
         LOG_ERROR << "There is no room in memory pool for the packet";
         return NULL;
     } else {
@@ -29,12 +24,7 @@ uint8_t *MemoryPool::allocatePacket(uint8_t *packet, uint16_t packetLength) {
     }
 }
 
-/**
- * This method is called when we want to delete the data of a packet.
- * @param packet pointer to the packet data in the pool.
- * @param packetLength length of the data.
- * @return true if the delete was successful and false if the packet was not found.
- */
+
 bool MemoryPool::deletePacket(uint8_t *packet, uint16_t packetLength) {
     int i = packet - &memory[0];
     if (i >= 0 && i <= memorySize - 1) {
@@ -45,15 +35,10 @@ bool MemoryPool::deletePacket(uint8_t *packet, uint16_t packetLength) {
     return false;
 }
 
-/**
- * This methods finds the first empty space in the memory pool that can fit the data
- * @param packetLength length of the data.
- * @return -1 if there was not enough space for the data else returns the index of the first memory
- * where the data will be stored.
- */
-int MemoryPool::findFit(uint16_t packetLength) {
-    int start = -1;
-    unsigned int currentWindow = 0;
+
+std::pair<uint16_t, MasterChannelAlert> MemoryPool::findFit(uint16_t packetLength) {
+    std::pair<uint16_t, MasterChannelAlert> index;
+    uint16_t currentWindow = 0;
     for (unsigned int i = 0; i < sizeof(memory) / sizeof(uint8_t); i++) {
         if (!usedMemory[i]) {
             currentWindow++;
@@ -61,12 +46,15 @@ int MemoryPool::findFit(uint16_t packetLength) {
             currentWindow = 0;
         }
         if (currentWindow == packetLength) {
-            start = i - packetLength + 1;
-            return start;
+            index.first = i - packetLength + 1;
+            index.second = NO_MC_ALERT;
+            return index;
         }
 
     }
-    return start;
+    index.first = 0;
+    index.second = NO_SPACE;
+    return index;
 }
 
 uint8_t *MemoryPool::getMemory() {
