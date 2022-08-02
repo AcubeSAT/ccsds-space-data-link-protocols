bfe4e0afbd9bd71280ec103c39d0647201d1f336
konkapo <kostaskapogl@gmail.com>
2022-08-02 12:15:09 +0300

    remove spare getters, move clcw field getters to CLCW.hpp

--- inc/CLCW.hpp
+++ inc/CLCW.hpp
@@ -14,61 +14,54 @@ struct CLCW {
 	           retransmit << 11U | farmBCounter << 9U | spare2 << 8U | reportValue){};
 
 public:
-	const uint32_t getClcw();
+	const uint32_t getClcw() {
+		return clcw;
+	}
 
 	/**
 	 * Control Word Type is set to 0 to indicate that the Operational Control Field contains a CLCW
 	 * @return bit 0 of the CLCW
 	 * @see 4.2.2 from Telecommand Part 2
 	 */
-	const bool getControlWordType();
+	const bool getControlWordType() {
+		return clcw >> 31U;
+	}
 
 	/**
 	 * Clcw Version Number is set to value "00"  to indicate the Version-1 CLCW is used.
 	 * @return bits 1,2 of the CLCW
 	 * @see 4.2.2 from Telecommand Part 2
 	 */
-	const uint8_t getClcwVersion();
+	const uint8_t getClcwVersion() {
+		return (clcw >> 29U) & 0x3;
+	}
 
 	/**
 	 * Field status is mission-specific and not used in the CCSDS data link protocol
 	 * @return Bits 3-5 of the CLCW (the Status Field).
 	 * @see p. 4.2.1.4 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const uint8_t getStatusField();
-
-	/**
-    *Bits 14-15 of the CLCW shall contain the Reserved Spare. Shall be set to ‘00’
-    * @return bits 14,15 of the CLCW
-    * @see 4.2.2 from Telecommand Part 2
-	 */
-	const uint8_t getSpare() {
-		return (clcw >> 16U) & 0x2;
+	const uint8_t getStatusField() {
+		return (clcw >> 26U) & 0x7;
 	}
-
-	/**
-    * Bit 23 of the CLCW shall contain the Reserved Spare. Shall be set to 0
-    * @return bit 23 of the CLCW
-    * @see 4.2.2 from Telecommand Part 2
-	 */
-	const uint8_t getSpareSecondPart() {
-		return (clcw >> 8U) & 0x1;
-	}
-
 
 	/**
 	 * Used to indicate the COP that is being used.
 	 * @return Bits 6-7 of the CLCW (the COP in Effect parameter)
 	 * @see p. 4.2.1.5 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const uint8_t getCopInEffect();
+	const uint8_t getCopInEffect() {
+		return (clcw >> 24U) & 0x3;
+	}
 
 	/**
 	 * @return Bits 8-13 of the CLCW (the Virtual Channel Identifier of the Virtual Channel
 	 * with which this COP report is associated).
 	 * @see p. 4.2.1.6 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const uint8_t getVcId();
+	const uint8_t getVcId() {
+		return (clcw >> 18U) & 0x3F;
+	}
 
 	/**
 	 * The No RF Available Flag shall provide a logical indication of the ‘ready’ status
@@ -76,57 +69,73 @@ public:
 	 * @return Bit 16 of the CLCW (the No RF Available Flag).
 	 * @see p. 4.2.1.8.2 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const bool getNoRfAvailable();
+	const bool getNoRfAvailable() {
+		return (clcw >> 15U) & 0x1;
+	}
 
 	/**
 	 * Indicates whether "bit lock" is achieved
 	 * @return Bit 17 of the CLCW (the No Bit Lock Flag).
 	 * @see p. 4.2.1.8.3 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const bool getNoBitLock();
+	const bool getNoBitLock() {
+		return (clcw >> 14U) & 0x1;
+	}
 
 	/**
 	 * Indicates whether FARM is in the Lockout state
 	 * @return Bit 18 of the CLCW (the Lockout Flag).
 	 * @see p. 4.2.1.8.4 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const bool getLockout();
+	const bool getLockout() {
+		return (clcw >> 13U) & 0x1;
+	}
 
 	/**
 	 * Indicates whether the receiver doesn't accept any packets
 	 * @return Bit 19 of the CLCW (the Wait Flag)
 	 * @see p. 4.1.2.8.5 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const bool getWait();
+	const bool getWait() {
+		return (clcw >> 12U) & 0x1;
+	}
 
 	/**
 	 * Indicates whether there are Type-A Frames that will need to be retransmitted
 	 * @return Bit 20 of the CLCW (the Retransmit Flag).
 	 * @see p. 4.2.1.8.6 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const bool getRetransmit();
+	const bool getRetransmit() {
+		return (clcw >> 11U) & 0x1;
+	}
 
 	/**
 	 * The two least significant bits of the virtual channel
 	 * @return Bits 21-22 of the CLCW (the FARM-B Counter).
 	 * @see p. 4.2.1.9 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const uint8_t getFarmBCounter();
+	const uint8_t getFarmBCounter() {
+		return (clcw >> 9U) & 0x3;
+	}
+
 
 	/**
 	 * Next expected transfer frame number
 	 * @return Bits 24-31 of the CLCW (the Report Value).
 	 * @see p. 4.2.11.1 from TC SPACE DATA LINK PROTOCOL
 	 */
-	const uint8_t getReportValue();
+	const uint8_t getReportValue() {
+		return clcw & 0xFF;
+	}
+
 
 
 	void setCLCW(CLCW newclcw) {
 		clcw = (newclcw.getControlWordType() << 31U | newclcw.getClcwVersion() << 29U |
 		        newclcw.getStatusField() << 26U | newclcw.getCopInEffect() << 24U | newclcw.getVcId() << 18U |
-		        newclcw.getSpare() << 16U | newclcw.getNoRfAvailable() << 15U | newclcw.getNoBitLock() << 14U |
+		        0 << 16U | newclcw.getNoRfAvailable() << 15U | newclcw.getNoBitLock() << 14U |
 		        newclcw.getLockout() << 13U | newclcw.getWait() << 12U |
-		        newclcw.getRetransmit() << 11U | newclcw.getFarmBCounter() << 9U | newclcw.getSpareSecondPart() << 8U |
+		        newclcw.getRetransmit() << 11U | newclcw.getFarmBCounter() << 9U | 0 << 8U |
 		        newclcw.getReportValue());
 
 	}
