453a0e1d6e165a40d666f21b633931d104f10ad3
konkapo <kostaskapogl@gmail.com>
2022-09-04 18:08:40 +0300

    Add frame buffers

--- test/TmFunctionalTesting.cpp
+++ test/TmFunctionalTesting.cpp
@@ -18,7 +18,7 @@ TEST_CASE("Sending TM"){
 	masterChannel.addVC(1, 128, false,3, 2, true, false, 0, true, SynchronizationFlag::FORWARD_ORDERED,255 , 20, 20,
 	                    10);
 
-	masterChannel.addVC(2, 128, false,3, 2, false, false, 0, true, SynchronizationFlag::FORWARD_ORDERED,255 , 3, 3,
+	masterChannel.addVC(2, 128, false,3, 2, true, false, 0, true, SynchronizationFlag::FORWARD_ORDERED,255 , 3, 3,
 	                    10);
 
 
@@ -32,12 +32,15 @@ TEST_CASE("Sending TM"){
 	etl::queue<uint16_t, PacketBufferTmSize> packetsVC0Length;
 	etl::queue<uint16_t, PacketBufferTmSize> packetsVC1Length;
 	etl::queue<uint16_t, PacketBufferTmSize> packetsVC2Length;
-
+	etl::queue<uint16_t, PacketBufferTmSize> framesSent;
+	etl::queue<uint16_t, PacketBufferTmSize> framesSentLength;
+	uint8_t trailerLength = 6;
 	uint8_t maximumPacketLength = 45;
-	uint8_t maximumFrameLength = maximumPacketLength+14;
+	uint8_t maximumFrameLength = maximumPacketLength+12;
 	uint8_t frame[maximumFrameLength];
 	uint8_t headerLength = 6;
 	uint8_t numberOfErrors = 0;
+	uint8_t headerAndTrailerLength = headerLength+trailerLength;
     for(uint8_t i=0 ; i<50 ; i++) {
 		uint8_t frameLength = (rand() % maximumFrameLength);
 		for (uint8_t j = 0; j < 6; j++) {
@@ -51,9 +54,9 @@ TEST_CASE("Sending TM"){
 			uint8_t randomServicePicker = rand() % 4;
 			if (randomServicePicker == 0) {
 				serviceChannel.storePacketTm(frame, frameLength, 0);
+				packetsVC0Length.push(frameLength - headerAndTrailerLength);
 				for (int j = 6; j < frameLength - 8; ++j) {
 					packetsVC0.push(frame[j]);
-					packetsVC0Length.push(frameLength - 14);
 				}
 			} else if (randomServicePicker == 1) {
 				serviceChannel.vcGenerationService(60, 0);
@@ -61,14 +64,18 @@ TEST_CASE("Sending TM"){
 				serviceChannel.mcGenerationTMRequest();
 			} else if (randomServicePicker == 3) {
 				serviceChannel.allFramesGenerationTMRequest(packetDestination, TmTransferFrameSize);
+				framesSentLength.push(frameLength);
+				for (int j = 0; j < frameLength; ++j) {
+					framesSent.push(packetDestination2[j]);
+				}
 			}
 		} else if (virtualChannelPicker > 50 && virtualChannelPicker < 60) {
 			uint8_t randomServicePicker = rand() % 4;
 			if (randomServicePicker == 0) {
 				serviceChannel.storePacketTm(frame, frameLength, 1);
-				for (int j = 6; j < frameLength - 8; ++j) {
+				packetsVC1Length.push(frameLength - headerAndTrailerLength);
+				for (int j = headerLength; j < frameLength - trailerLength; ++j) {
 					packetsVC1.push(frame[j]);
-					packetsVC1Length.push(frameLength - 14);
 				}
 			} else if (randomServicePicker == 1) {
 				serviceChannel.vcGenerationService(60, 1);
@@ -76,14 +83,18 @@ TEST_CASE("Sending TM"){
 				serviceChannel.mcGenerationTMRequest();
 			} else if (randomServicePicker == 3) {
 				serviceChannel.allFramesGenerationTMRequest(packetDestination, TmTransferFrameSize);
+				framesSentLength.push(frameLength);
+				for (int j = 0; j < frameLength; ++j) {
+					framesSent.push(packetDestination[j]);
+				}
 			}
 		} else {
 			uint8_t randomServicePicker = rand() % 4;
 			if (randomServicePicker == 0) {
 				serviceChannel.storePacketTm(frame, frameLength, 2);
-				for (int j = 6; j < frameLength - 8; ++j) {
+				packetsVC2Length.push(frameLength - headerAndTrailerLength);
+				for (int j = headerLength; j < frameLength - trailerLength; ++j) {
 					packetsVC2.push(frame[j]);
-					packetsVC2Length.push(frameLength - 14);
 				}
 			} else if (randomServicePicker == 1) {
 				serviceChannel.vcGenerationService(60, 2);
@@ -91,6 +102,10 @@ TEST_CASE("Sending TM"){
 				serviceChannel.mcGenerationTMRequest();
 			} else if (randomServicePicker == 3) {
 				serviceChannel.allFramesGenerationTMRequest(packetDestination, TmTransferFrameSize);
+				framesSentLength.push(frameLength);
+				for (int j = 0; j < frameLength; ++j) {
+					framesSent.push(packetDestination[j]);
+				}
 			}
 		}
 		if (packetDestination[0] != 0) {
@@ -99,48 +114,82 @@ TEST_CASE("Sending TM"){
 				if (virtualChannelPicker <= 50) {
 					uint8_t randomServicePicker2 = rand() % 2;
 					if (randomServicePicker2 == 0) {
-						serviceChannel.allFramesReceptionTMRequest(packetDestination2, TmTransferFrameSize);
+						if(!framesSentLength.empty()) {
+							uint8_t tmplength = framesSentLength.front();
+							framesSentLength.pop();
+							for (uint16_t t = 0; t < tmplength; t++) {
+								tmpData[t] = framesSent.front();
+								framesSent.pop();
+							}
+							serviceChannel.allFramesReceptionTMRequest(tmpData, TmTransferFrameSize);
+						}
 					} else if (randomServicePicker2 == 1) {
 						serviceChannel.packetExtractionTM(0, packetDestination2);
 						if (packetDestination2[0] != 0) {
-							uint8_t tmplength = packetsVC0Length.front();
-							for (uint16_t t = 0; t < tmplength; t++) {
-								tmpData[t] = packetsVC0.front();
-								packetsVC0.pop();
+							if (!packetsVC0Length.empty()) {
+								uint8_t tmplength = packetsVC0Length.front();
+								packetsVC0Length.pop();
+								for (uint16_t t = 0; t < tmplength; t++) {
+									tmpData[t] = packetsVC0.front();
+									packetsVC0.pop();
+								}
 							}
 						}
 					}
 				} else if (virtualChannelPicker > 50 && virtualChannelPicker < 60) {
 					uint8_t randomServicePicker2 = rand() % 2;
 					if (randomServicePicker2 == 0) {
-						serviceChannel.allFramesReceptionTMRequest(frame, TmTransferFrameSize);
+						if (!framesSentLength.empty()) {
+							uint8_t tmplength = framesSentLength.front();
+							framesSentLength.pop();
+							for (uint16_t t = 0; t < tmplength; t++) {
+								tmpData[t] = framesSent.front();
+								framesSent.pop();
+							}
+							serviceChannel.allFramesReceptionTMRequest(tmpData, TmTransferFrameSize);
+						}
+
 					} else if (randomServicePicker2 == 1) {
 						serviceChannel.packetExtractionTM(1, packetDestination2);
 						if (packetDestination2[0] != 0) {
-							uint8_t tmplength = packetsVC0Length.front();
-							for (uint16_t t = 0; t < tmplength; t++) {
-								tmpData[t] = packetsVC0.front();
-								packetsVC0.pop();
+							if (!packetsVC1Length.empty()) {
+								uint8_t tmplength = packetsVC1Length.front();
+								packetsVC1Length.pop();
+								for (uint16_t t = 0; t < tmplength; t++) {
+									tmpData[t] = packetsVC1.front();
+									packetsVC1.pop();
+								}
 							}
 						}
 					}
 				} else {
 					uint8_t randomServicePicker2 = rand() % 2;
 					if (randomServicePicker2 == 0) {
-						serviceChannel.allFramesReceptionTMRequest(frame, TmTransferFrameSize);
+						if (!framesSentLength.empty()) {
+							uint8_t tmplength = framesSentLength.front();
+							framesSentLength.pop();
+							for (uint16_t t = 0; t < tmplength; t++) {
+								tmpData[t] = framesSent.front();
+								framesSent.pop();
+							}
+							serviceChannel.allFramesReceptionTMRequest(tmpData, TmTransferFrameSize);
+						}
 					} else if (randomServicePicker2 == 1) {
 						serviceChannel.packetExtractionTM(2, packetDestination2);
 						if (packetDestination2[0] != 0) {
-							uint8_t tmplength = packetsVC0Length.front();
-							for (uint16_t t = 0; t < tmplength; t++) {
-								tmpData[t] = packetsVC0.front();
-								packetsVC0.pop();
+							if (!packetsVC2Length.empty()) {
+								uint8_t tmplength = packetsVC2Length.front();
+								packetsVC2Length.pop();
+								for (uint16_t t = 0; t < tmplength; t++) {
+									tmpData[t] = packetsVC2.front();
+									packetsVC2.pop();
+								}
 							}
 						}
 					}
 				}
 				if (packetDestination2[0] != 0) {
-					for (uint8_t k = 0; k < frameLength - 14; ++k) {
+					for (uint8_t k = 0; k < frameLength - headerAndTrailerLength; ++k) {
 						if (tmpData[k] != packetDestination2[k]) {
 							numberOfErrors++;
 						}
